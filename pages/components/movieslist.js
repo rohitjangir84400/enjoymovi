1f4e77be239024ea5cf1df929b511e264b964c34
Rohit Jangir <114482963+rohitjangir84400@users.noreply.github.com>
2022-10-05 00:54:40 +0530

    added tabs to the movie and tv pages

--- pages/components/movieslist.js
+++ pages/components/movieslist.js
@@ -1,66 +1,101 @@
 import Image from "next/image";
 import Link from "next/link";
+import { TabPanel, useTabs } from "react-headless-tabs";
 
-export default function MovieList({ movie , toprated , upcoming }) {
+export default function MovieList({ movie, toprated, upcoming }) {
+
+  const [selectedTab, setSelectedTab] = useTabs([
+    "upcoming",
+    "popular",
+    "top_rated",
+  ]);
+
   return (
     <>
-    <div className="text-[30px] bg-black text-[#00ffff] py-5 pl-5">Upcoming Movies</div>
-    <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-1 gap-2 bg-black p-5">
-      {upcoming.map(movie => {
-        return (
-          <div key={movie.id} className="hover:cursor-pointer hover:scale-90 transition duration-700 ease-in-out">
-            <Link href={`/upcomingmovies/${movie.id}`}>
-              <div>
-                <div className="bg-white rounded-3xl">
-                  <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} aly='' height={750} width={500} className="rounded-t-3xl" />
-                  <div className="px-2 text-black text-center h-[50px] rounded-b-3xl">{movie.title}</div>
+      <div className="flex text-center">
+        <div className="grow"
+          isActive={selectedTab === "upcoming"}
+          onClick={() => setSelectedTab("upcoming")}
+        >
+          Upcoming
+        </div>
+        <div className="grow"
+          isActive={selectedTab === "popular"}
+          onClick={() => setSelectedTab("popular")}
+        >
+          Popular
+        </div>
+        <div className="grow"
+          isActive={selectedTab === "top_rated"}
+          onClick={() => setSelectedTab("top_rated")}
+        >
+          Toprated
+        </div>
+      </div>
+
+      
+      <div>
+        <TabPanel hidden={selectedTab !== "upcoming"}>
+          <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-1 gap-2 bg-black p-5">
+            {upcoming.map(movie => {
+              return (
+                <div key={movie.id} className="hover:cursor-pointer hover:scale-90 transition duration-700 ease-in-out">
+                  <Link href={`/upcomingmovies/${movie.id}`}>
+                    <div>
+                      <div className="bg-white rounded-3xl">
+                        <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} aly='' height={750} width={500} className="rounded-t-3xl" />
+                        <div className="px-2 text-black text-center h-[50px] rounded-b-3xl">{movie.title}</div>
+                      </div>
+
+                    </div>
+                  </Link>
                 </div>
 
-              </div>
-            </Link>
+              );
+            })}
           </div>
+        </TabPanel>
+        <TabPanel hidden={selectedTab !== "popular"}>
+          <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-1 gap-2 bg-black p-5">
+            {movie.map(movie => {
+              return (
+                <div key={movie.id} className="hover:cursor-pointer hover:scale-90 transition duration-700 ease-in-out">
+                  <Link href={`/movie/${movie.id}`}>
+                    <div>
+                      <div className="bg-white rounded-3xl">
+                        <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} aly='' height={750} width={500} className="rounded-t-3xl" />
+                        <div className="px-2 text-black text-center h-[50px] rounded-b-3xl">{movie.title}</div>
+                      </div>
 
-        );
-      })}
-    </div>
-    <div className="text-[30px] bg-black text-[#00ffff] py-5 pl-5">Popular Movies</div>
-    <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-1 gap-2 bg-black p-5">
-      {movie.map(movie => {
-        return (
-          <div key={movie.id} className="hover:cursor-pointer hover:scale-90 transition duration-700 ease-in-out">
-            <Link href={`/movie/${movie.id}`}>
-              <div>
-                <div className="bg-white rounded-3xl">
-                  <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} aly='' height={750} width={500} className="rounded-t-3xl" />
-                  <div className="px-2 text-black text-center h-[50px] rounded-b-3xl">{movie.title}</div>
+                    </div>
+                  </Link>
                 </div>
 
-              </div>
-            </Link>
+              );
+            })}
           </div>
+        </TabPanel>
+        <TabPanel hidden={selectedTab !== "top_rated"}>
+          <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-1 gap-2 bg-black p-5">
+            {toprated.map(movie => {
+              return (
+                <div key={movie.id} className="hover:cursor-pointer hover:scale-90 transition duration-700 ease-in-out">
+                  <Link href={`/tmovie/${movie.id}`}>
+                    <div>
+                      <div className="bg-white rounded-3xl">
+                        <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} aly='' height={750} width={500} className="rounded-t-3xl" />
+                        <div className="px-2 text-black text-center h-[50px] rounded-b-3xl">{movie.title}</div>
+                      </div>
 
-        );
-      })}
-    </div>
-    <div className="text-[30px] bg-black text-[#00ffff] py-5 pl-5">Toprated Movies</div>
-    <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-1 gap-2 bg-black p-5">
-      {toprated.map(movie => {
-        return (
-          <div key={movie.id} className="hover:cursor-pointer hover:scale-90 transition duration-700 ease-in-out">
-            <Link href={`/tmovie/${movie.id}`}>
-              <div>
-                <div className="bg-white rounded-3xl">
-                  <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} aly='' height={750} width={500} className="rounded-t-3xl" />
-                  <div className="px-2 text-black text-center h-[50px] rounded-b-3xl">{movie.title}</div>
+                    </div>
+                  </Link>
                 </div>
 
-              </div>
-            </Link>
+              );
+            })}
           </div>
-
-        );
-      })}
-    </div>
+        </TabPanel>
+      </div>
     </>
   );
 }
--- pages/components/tvlist.js
+++ pages/components/tvlist.js
@@ -1,41 +1,91 @@
 import Image from "next/image";
 import Link from "next/link";
+import { TabPanel, useTabs } from "react-headless-tabs";
 
-export default function TvList({ latesttvshow , topratedtv , populartv }) {
+
+export default function TvList({ latesttvshow, topratedtv, populartv }) {
+
+  const [selectedTab, setSelectedTab] = useTabs([
+    "popular",
+    "top_rated",
+  ]);
+
   return (
-   <>
-    <div className="text-[30px] bg-black text-[#00ffff] py-5 pl-5">Popular Tv</div>
-    <div className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-2 bg-black p-5">
-      {populartv.map(populartv => {
-        return (
-          <div key={populartv.id}className="hover:cursor-pointer hover:scale-90 transition duration-700 ease-in-out">
-            <Link href={`/tv/${populartv.id}`}>
-              <div className="bg-white rounded-3xl">
-                <Image src={`https://image.tmdb.org/t/p/w500${populartv.poster_path}`} alt='' height={750} width={500} className="rounded-t-3xl" />
-                <div className="px-2 text-black text-centerx h-[50px] rounded-b-3xl">{populartv.name}</div>
-              </div>
-            </Link>
+    <>
+      <div className="flex text-center">
+        <div className="grow"
+          isActive={selectedTab === "popular"}
+          onClick={() => setSelectedTab("popular")}
+        >
+          Popular
+        </div>
+        <div className="grow"
+          isActive={selectedTab === "top_rated"}
+          onClick={() => setSelectedTab("top_rated")}
+        >
+          Toprated
+        </div>
+      </div>
+
+      <div>
+        {/* <TabPanel hidden={selectedTab !== "upcoming"}>
+          <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-1 gap-2 bg-black p-5">
+            {upcoming.map(movie => {
+              return (
+                <div key={movie.id} className="hover:cursor-pointer hover:scale-90 transition duration-700 ease-in-out">
+                  <Link href={`/upcomingmovies/${movie.id}`}>
+                    <div>
+                      <div className="bg-white rounded-3xl">
+                        <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} aly='' height={750} width={500} className="rounded-t-3xl" />
+                        <div className="px-2 text-black text-center h-[50px] rounded-b-3xl">{movie.title}</div>
+                      </div>
+
+                    </div>
+                  </Link>
+                </div>
+
+              );
+            })}
           </div>
-        );
-      })}
-    </div>
-    <div className="text-[30px] bg-black text-[#00ffff] py-5 pl-5">Toprated Tv</div>
-    <div className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-2 bg-black p-5">
-      {topratedtv.map(toptv => {
-        return (
-          <div key={toptv.id}className="hover:cursor-pointer hover:scale-90 transition duration-700 ease-in-out">
-            <Link href={`/toptv/${toptv.id}`}>
-              <div className="bg-white rounded-3xl">
-                <Image src={`https://image.tmdb.org/t/p/w500${toptv.poster_path}`} alt='' height={750} width={500} className="rounded-t-3xl" />
-                <div className="px-2 text-black text-centerx h-[50px] rounded-b-3xl">{toptv.name}</div>
-              </div>
-            </Link>
+        </TabPanel> */}
+        <TabPanel hidden={selectedTab !== "popular"}>
+          <div className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-2 bg-black p-5">
+            {populartv.map(populartv => {
+              return (
+                <div key={populartv.id} className="hover:cursor-pointer hover:scale-90 transition duration-700 ease-in-out">
+                  <Link href={`/tv/${populartv.id}`}>
+                    <div className="bg-white rounded-3xl">
+                      <Image src={`https://image.tmdb.org/t/p/w500${populartv.poster_path}`} alt='' height={750} width={500} className="rounded-t-3xl" />
+                      <div className="px-2 text-black text-centerx h-[50px] rounded-b-3xl">{populartv.name}</div>
+                    </div>
+                  </Link>
+                </div>
+              );
+            })}
           </div>
-        );
-      })}
-    </div>
-    <div className="text-[30px] bg-black text-[#00ffff] py-5 pl-5">Latest Tv</div>
-    <div className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-2 bg-black p-5">
+        </TabPanel>
+        <TabPanel hidden={selectedTab !== "top_rated"}>
+          <div className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-2 bg-black p-5">
+            {topratedtv.map(toptv => {
+              return (
+                <div key={toptv.id} className="hover:cursor-pointer hover:scale-90 transition duration-700 ease-in-out">
+                  <Link href={`/toptv/${toptv.id}`}>
+                    <div className="bg-white rounded-3xl">
+                      <Image src={`https://image.tmdb.org/t/p/w500${toptv.poster_path}`} alt='' height={750} width={500} className="rounded-t-3xl" />
+                      <div className="px-2 text-black text-centerx h-[50px] rounded-b-3xl">{toptv.name}</div>
+                    </div>
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        </TabPanel>
+      </div>
+    </>
+  );
+}
+
+{/* <div className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-2 bg-black p-5">
       {latesttvshow?.map(latesttv => {
         return (
           <div key={latesttv.id}className="hover:cursor-pointer hover:scale-90 transition duration-700 ease-in-out">
@@ -48,8 +98,4 @@ export default function TvList({ latesttvshow , topratedtv , populartv }) {
           </div>
         );
       })}
-    </div>
-   </>
-  );
-}
-
+    </div> */}
