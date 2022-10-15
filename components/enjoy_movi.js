a9d468e34c600b7c7d9cc69ae194d8295b428837
Rohit Jangir <114482963+rohitjangir84400@users.noreply.github.com>
2022-10-16 00:17:46 +0530

    minor change

--- components/enjoy_movi.js
+++ components/enjoy_movi.js
@@ -7,7 +7,7 @@ export default function Movi() {
             <div className="flex flex-wrap items-center">
                 <div className="md:basis-1/2 basis-12/12 md:border-r-4 border-dashed px-5">
                     <div className="text-[40px] font-bold py-5">enjoymovi</div>
-                    <div className="font-thin">
+                    <div className="font-thin text-center sm:text-left">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     </div>
                 </div>
