1885c28307ff5d9c0c546c5c2af1e55722738f37
Rohit Jangir <114482963+rohitjangir84400@users.noreply.github.com>
2022-10-16 00:21:17 +0530

    minor

--- components/enjoy_movi.js
+++ components/enjoy_movi.js
@@ -5,8 +5,8 @@ export default function Movi() {
     return (
         <div className="p-10 text-white">
             <div className="flex flex-wrap items-center">
-                <div className="md:basis-1/2 basis-12/12 md:border-r-4 border-dashed px-5">
-                    <div className="text-[40px] font-bold py-5 text-center sm:text-left">enjoymovi</div>
+                <div className="md:basis-1/2 basis-12/12 md:border-r-4 border-dashed px-5 text-center sm:text-left">
+                    <div className="text-[40px] font-bold py-5 ">enjoymovi</div>
                     <div className="font-thin ">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     </div>
