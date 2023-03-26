74b53240044aa69eae955400a440fc7fcd59a2fc
Rohit Jangir <114482963+rohitjangir84400@users.noreply.github.com>
2023-03-26 20:48:56 +0530

    config file change

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   images:{
     domains:['image.tmdb.org']
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
