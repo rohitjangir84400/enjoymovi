9cd7246f55f7ff70dbfe92c0bda6902ae4ddeeca
Rohit Jangir <114482963+rohitjangir84400@users.noreply.github.com>
2023-03-26 20:55:06 +0530

    Revert "config file change"
    This reverts commit 74b53240044aa69eae955400a440fc7fcd59a2fc.

--- next.config.js
+++ next.config.js
@@ -5,11 +5,6 @@ const nextConfig = {
   images:{
     domains:['image.tmdb.org']
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 }
 
 module.exports = nextConfig
